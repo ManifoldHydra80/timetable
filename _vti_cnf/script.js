8de06775d79b5764453a10dbb911c3aef5de4e93
GITJaliJali84 <91525267+ManifoldHydra80@users.noreply.github.com>
2022-01-30 17:14:53 +1100

    Initial commit

---
+++ _vti_cnf/script.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|30 Jan 2022 03:51:12 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|SDC\\ING0006
+vti_modifiedby:SR|SDC\\ING0006
+vti_timecreated:TR|30 Jan 2022 03:51:12 -0000
+vti_cacheddtm:TX|30 Jan 2022 03:51:12 -0000
+vti_filesize:IR|11
+vti_backlinkinfo:VX|index.html
---
+++ script.js
@@ -0,0 +1,29 @@
+Timetable = {}
+Timetable.day = {}
+Timetable.Weekday = new Date().getDay()
+
+Timetable.day[1] = {Day:"Monday",Homegroup:"Homegroup",Session1:"Work Related Skills",Session2:"Work Related Skills",Recess:"Recess",Session3:"Literacy",Session4:"VET",Lunch:"Lunch",Session5:"Pastoral Care",Session6:"Maths"}
+Timetable.day[2] = {Day:"Tuesday",Homegroup:"Homegroup",Session1:"Parsonal Development Skills",Session2:"Parsonal Development Skills",Recess:"Recess",Session3:"VET",Session4:"VET",Lunch:"Lunch",Session5:"Maths",Session6:"Maths"}
+Timetable.day[4] = {Day:"Thursday",Homegroup:"Homegroup",Session1:"Maths",Session2:"Maths",Recess:"Recess",Session3:"Literacy",Session4:"Literacy",Lunch:"Lunch",Session5:"Parsonal Development Skills",Session6:"Parsonal Development Skills"}
+Timetable.day[5] = {Day:"Friday",Homegroup:"Homegroup",Session1:"Literacy",Session2:"Literacy",Recess:"Recess",Session3:"Work Related Skills",Session4:"Work Related Skills",Lunch:"Lunch",Session5:"VET",Session6:"VET"}
+
+// Setting Up The Timetable
+Timetable.SetTimetable=function ()
+{
+	if (Timetable.Weekday >= 1 && Timetable.Weekday <= 5) {
+		document.getElementById("error").innerHTML = ""
+		document.getElementById("weekday").innerHTML = Timetable.day[DayNumber].Day
+		document.getElementById("homegroup").innerHTML = Timetable.day[DayNumber].Homegroup
+		document.getElementById("session1").innerHTML = Timetable.day[DayNumber].Session1
+		document.getElementById("session2").innerHTML = Timetable.day[DayNumber].Session2
+		document.getElementById("recess").innerHTML = Timetable.day[DayNumber].Recess
+		document.getElementById("session3").innerHTML = Timetable.day[DayNumber].Session3
+		document.getElementById("session4").innerHTML = Timetable.day[DayNumber].Session4
+		document.getElementById("lunch").innerHTML = Timetable.day[DayNumber].Lunch
+		document.getElementById("session5").innerHTML = Timetable.day[DayNumber].Session5
+		document.getElementById("session6").innerHTML = Timetable.day[DayNumber].Session6
+	} else {
+		document.getElementById("error").innerHTML = "Sorry, This Only Works On School Days"
+	}
+}
+Timetable.SetTimetable()
